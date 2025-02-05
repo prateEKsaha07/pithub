f08fd133696e7f702205603ffbbc52795b29658b
prateEKsaha07 <155552003+prateEKsaha07@users.noreply.github.com>
2025-02-05 23:02:12 +0530

    listing steps

--- pithub/pithub/settings.py
+++ pithub/pithub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'project',
     'account',
+    'todolist',
     'core', 
 ]
 
--- pithub/todolist/urls.py
+++ pithub/todolist/urls.py
@@ -3,13 +3,12 @@ from django.urls import path
 from . import views
 
 
-app_name = 'project'
+app_name = 'todolist'
 
 
 urlpatterns = [
-    path('', views.projects, name='projects'),
     path('add/', views.add_project, name='add'),
-    path('<uuid:pk>/', views.project, name='project'),
+    path('<uuid:pk>/', views.todolist, name='todolist'),
     path('<uuid:pk>/edit/', views.edit, name='edit'),
     path('<uuid:pk>/delete/', views.delete, name='delete'),
 ]
