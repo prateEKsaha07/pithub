20d09a0949d0b0b38a5dfa9258163eede5467103
prateEKsaha07 <155552003+prateEKsaha07@users.noreply.github.com>
2025-02-05 21:13:04 +0530

    hju

--- pithub/pithub/urls.py
+++ pithub/pithub/urls.py
@@ -4,4 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('', include('account.urls')),
+    path('projects/', include('project.urls')),
 ]
